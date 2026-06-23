dd507cb26a6730eaddd03ed0c0bc8455708bf8b3
Lipe-meira <felipemeira018@gmail.com>
2026-06-23 10:42:04 -0300

    feat: permite marcar compras como concluídas

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,60 @@ import { useState } from "react";
 import Compras from "./components/listaCompras/Compras";
 
 function App() {
-                  //implementar depois 
-  const [compras, _setCompras] = useState([
-      {
-        nome: "leite",
-        valor: 2.99,
-        quantidade: 5,
-      },
-      {
-        nome: "pão",
-        valor: 5.99,
-        quantidade: 5,
-      },
-      {
-        nome: "arroz",
-        valor: 15.99,
-        quantidade: 2,
-      },
-      {
-        nome: "feijão",
-        valor: 10.99,
-        quantidade: 1,
-      },
-      {
-        nome: "carne",
-        valor: 29.99,
-        quantidade: 5,
-      },
-      {
-        nome: "frango",
-        valor: 19.99,
-        quantidade: 5,
-      },
-    
+  const [compras, setCompras] = useState([
+    {
+      id: 1,
+      nome: "leite",
+      valor: 2.99,
+      comprado: false,
+      quantidade: 5,
+    },
+    {
+      id: 2,
+      nome: "pão",
+      valor: 5.99,
+      comprado: false,
+      quantidade: 5,
+    },
+    {
+      id: 3,
+      nome: "arroz",
+      valor: 15.99,
+      comprado: false,
+      quantidade: 2,
+    },
+    {
+      id: 4,
+      nome: "feijão",
+      valor: 10.99,
+      comprado: false,
+      quantidade: 1,
+    },
+    {
+      id: 5,
+      nome: "carne",
+      valor: 29.99,
+      quantidade: 5,
+      comprado: false,
+    },
+    {
+      id: 6,
+      nome: "frango",
+      valor: 19.99,
+      quantidade: 5,
+      comprado: false,
+    },
   ]);
+
+  function jaComprado(compraId) {
+    setCompras((comprasAtuais) =>
+      comprasAtuais.map((compra) =>
+        compra.id === compraId
+          ? { ...compra, comprado: !compra.comprado }
+          : compra,
+      ),
+    );
+  }
 
   return (
     <div className="w-full min-h-screen bg-slate-500 flex justify-center p-6">
@@ -43,7 +63,7 @@ function App() {
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Lista de compras
         </h1>
-        <Compras compras={compras} />
+        <Compras compras={compras} jaComprado={jaComprado} />
       </div>
     </div>
   );
--- src/components/listaCompras/Compras.jsx
+++ src/components/listaCompras/Compras.jsx
@@ -4,8 +4,22 @@ function Compras(props) {
   return (
     <div className="lista-compras">
       {props.compras.map((compra) => (
-        <div key={compra.nome} className="card-compra">
-          <p className="nome-compra">{compra.nome}</p>
+        <div key={compra.id} className="card-compra">
+          <button
+            type="button"
+            onClick={() => props.jaComprado(compra.id)}
+            className="bg-slate-400 grow text-white p-2 rounded-md"
+          >
+            <span
+              className={
+                compra.comprado
+                  ? "text-red-500 ml-2 line-through"
+                  : "text-green-500 ml-2"
+              }
+            >
+              {compra.nome}
+            </span>
+          </button>
           <p>{compra.valor}</p>
           <p>{compra.quantidade}</p>
         </div>
